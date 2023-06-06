bcb8fd8747e92c2d1cb7c3c50ffec5a40000c914
nathanbassey <nathanbassey@gmail.com>
2023-06-06 12:46:26 -0400

    Fetch tasks from Local Storage

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const taskInput = document.getElementById("taskInput")
 const addTaskButton = document.getElementById("addTaskButton")
 const taskList = document.getElementById("taskList")
 
-// Load Tasks from Local Storage
+// 2. Load Tasks from Local Storage
 let taskArray = getTasksFromLocalStorage()
+
+// 3. Create Functions for Task Operations
+ function getTasksFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("tasks")) || []  
+ }
