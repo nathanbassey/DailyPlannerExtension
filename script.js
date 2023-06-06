98df3c518c1a07726e401646ea10a234be9ffa82
nathanbassey <nathanbassey@gmail.com>
2023-06-06 12:53:02 -0400

    Update tasks in local storage

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const taskList = document.getElementById("taskList")
 // 2. Load Tasks from Local Storage
 let taskArray = getTasksFromLocalStorage()
 
-// 3. Create Functions for Task Operations
+// 3.1 Create Functions for Task Operations
  function getTasksFromLocalStorage() {
     return JSON.parse(localStorage.getItem("tasks")) || []  
+ }
+
+//  3.2 Implement the upateTasksInLocalStorage
+ function updateTasksInLocalStorage() {
+   localStorage.setItem("tasks", JSON.stringify(taskArray))
  }
