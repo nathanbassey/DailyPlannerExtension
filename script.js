83b77c6722bf66eaa9931b0100bb4e5175307941
nathanbassey <nathanbassey@gmail.com>
2023-06-06 12:29:34 -0400

    Loaded tasks from Local Storage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const taskInput = document.getElementById("taskInput")
 const addTaskButton = document.getElementById("addTaskButton")
 const taskList = document.getElementById("taskList")
+
+// Load Tasks from Local Storage
+let taskArray = getTasksFromLocalStorage()
