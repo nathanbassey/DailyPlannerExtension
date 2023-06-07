0296a40c6504290925ab0fbdca5c66a992ae1977
nathanbassey <nathanbassey@gmail.com>
2023-06-07 11:31:42 -0400

    createTaskElement with taskObj parameter

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ function deleteTask(index) {
    taskArray.splice(index, 1) ; 
    updateTasksInLocalStorage()
 }
+
+// 3.5 Implement createTaskElement(taskObj) function
+
+   // function createTaskElement(taskObj) {
+   //    const li = document.createElement("li")
+   //    li.classList.add("taskItem")
+
+   //    const checkbox = document.createElement("input")
+   //    checkbox.type = "checkbox"
+
+   //    checkbox.classList.add("checkbox")
+
+   //    const taskTextElement = document.createElement("span")
+   // }
+
+   function createTaskElement(taskObj) {
+      const taskItem = document.createElement("li");
+      taskItem.classList.add("taskItem");
+    
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("checkbox");
+      checkbox.checked = taskObj.completed;
+      checkbox.addEventListener("change", function () {
+        taskObj.completed = checkbox.checked;
+        taskTextElement.classList.toggle("completed", taskObj.completed);
+        updateTasksInLocalStorage();
+      });
+    
+      const taskTextElement = document.createElement("span");
+      taskTextElement.classList.add("taskText");
+      taskTextElement.textContent = taskObj.text;
+      taskTextElement.classList.toggle("completed", taskObj.completed);
+    
+      const removeButton = document.createElement("button");
+      removeButton.classList.add("removeButton");
+      removeButton.textContent = "X";
+      removeButton.addEventListener("click", function () {
+        deleteTask(taskObj);
+        renderTasks();
+      });
+    
+      taskItem.appendChild(checkbox);
+      taskItem.appendChild(taskTextElement);
+      taskItem.appendChild(removeButton);
+    
+      return taskItem;
+    }
